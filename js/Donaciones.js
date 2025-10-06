374284af6759989716b54f605866030c71900dc8
ivan <ivansanchezdominguez1@gmail.com>
2025-10-06 19:40:27 +0200

    modificar los js y añadiendo el mostrarResumen

--- js/Donaciones.js
+++ js/Donaciones.js
@@ -1,21 +1,21 @@
-
 window.listaAportaciones = [];
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll("img[data-precio]").forEach(img => {
+  let imagenes = document.querySelectorAll("img[data-precio]");
+  
+  imagenes.forEach(img => {
     img.addEventListener("click", () => {
-      const nombre = img.alt;
-      const precio = parseFloat(img.getAttribute("data-precio")) || 0;
-     
-      const output = document.getElementById("output");
+      let nombre = img.alt;
+      let precio = parseFloat(img.getAttribute("data-precio")) || 0;
+
+      let output = document.getElementById("output");
       if (output) output.innerHTML = "";
 
       window.listaAportaciones.push({ organizacion: nombre, cantidad: precio });
     });
   });
 
- 
-  const boton = document.querySelector("button");
+  let boton = document.querySelector("button");
   if (boton) {
     boton.addEventListener("click", () => {
       window.mostrarTexto();
