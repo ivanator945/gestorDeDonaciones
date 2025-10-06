87871c1ff0432bb53346d35598bea18be519f022
ivan <ivansanchezdominguez1@gmail.com>
2025-10-06 19:55:13 +0200

    tabulando codigo

--- js/MostrarResumen.js
+++ js/MostrarResumen.js
@@ -4,31 +4,26 @@ window.mostrarTexto = function () {
   let totalDonado = resumenDonaciones.totalDonado;
 
   let listaNombres = [];
-  for (let nombreOrganizacion in conteoPorOrganizacion) {
-    listaNombres.push(nombreOrganizacion);
-  }
+    for (let nombreOrganizacion in conteoPorOrganizacion) {
+        listaNombres.push(nombreOrganizacion);
+    }
   listaNombres.sort().reverse();
 
   let resultado = "";
 
-  for (let nombre of listaNombres) {
-    let datos = conteoPorOrganizacion[nombre];
-
-    let palabraAportacion = datos.veces > 1 ? "aportaciones" : "aportación";
-
-    resultado +=
-    nombre + " ---- " + datos.veces + " " + palabraAportacion + "<br>";
-
-  }
+    for (let nombre of listaNombres) {
+        let datos = conteoPorOrganizacion[nombre];
+        let palabraAportacion = datos.veces > 1 ? "aportaciones" : "aportación";
+        resultado += nombre + " ---- " + datos.veces + " " + palabraAportacion + "<br>";
+    }
 
   var donacionMedia = 0;
-  if (window.listaAportaciones.length > 0) {
-  
-    donacionMedia = Math.round((totalDonado / window.listaAportaciones.length) * 100) / 100;
-  }
+    if (window.listaAportaciones.length > 0) {
+        donacionMedia = Math.round((totalDonado / window.listaAportaciones.length) * 100) / 100;
+    }
+
   resultado += "<br>Donación final: " + totalDonado + " €<br>";
   resultado += "Donación media: " + donacionMedia + " €/aportación";
-
   document.getElementById("output").innerHTML = resultado;
     
 }
